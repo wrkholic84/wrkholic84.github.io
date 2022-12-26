fb1dd38cfac07e3fffb2d5e1c20d478bcb276af7
spidey <wrkholic84@gmail.com>
2022-12-26 16:20:29 +0900

    Build test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221226.160444';
+const cacheName = 'chirpy-20221226.161725';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
